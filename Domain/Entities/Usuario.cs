dc1cd7705f9b0f6c0b44cc2068bf3c6a287992b2
Raydberg Gabriel <raydbergg@gmail.com>
2025-03-28 21:05:28 -0500

    New changes

--- Domain/Entities/Usuario.cs
+++ Domain/Entities/Usuario.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    internal class Usuario
+    public class Usuario
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Telefone { get; set; }
+        public string Cpf { get; set; }
+        public string Cnpj { get; set; }
+        public string Rg { get; set; }
+        public string Cnh { get; set; }
+        public string Endereco { get; set; }
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Pais { get; set; }
+        public string Complemento { get; set; }
+        public string Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Imagem { get; set; }
+        public string Tipo { get; set; }
+        public string Token { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public bool Ativo { get; set; }
     }
 }
