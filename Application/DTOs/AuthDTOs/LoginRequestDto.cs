e1da947333d34cc3aa23354a30adcf8b1cf10d23
Raydberg Gabriel <raydbergg@gmail.com>
2025-04-09 11:04:04 -0500

    feat: implement authentication endpoints for login and registration

---
+++ Application/DTOs/AuthDTOs/LoginRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.AuthDTOs;
+
+public class LoginRequestDto
+{
+    [Required(ErrorMessage = "El correo electrónico es obligatorio")]
+    [EmailAddress(ErrorMessage = "El correo electrónico no es válido")]
+    public required string Email { get; set; }
+
+    [Required(ErrorMessage = "La contraseña es obligatoria")]
+    public string? Password { get; set; }
+}
--- NeonNovaApp/Controllers/AuthController.cs
+++ NeonNovaApp/Controllers/AuthController.cs
@@ -1,11 +1,110 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Application.DTOs.AuthDTOs;
+using Domain.Entities;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.IdentityModel.Tokens;
 
 namespace NeonNovaApp.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/auth")]
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly UserManager<Users> _userManager;
+        private readonly SignInManager<Users> _signInManager;
+
+        public AuthController(UserManager<Users> userManager,
+            SignInManager<Users> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AuthenticationResponseDto>> Register(CredentialsUserDto dto)
+        {
+            var user = new Users
+            {
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+                Email = dto.Email,
+                UserName = dto.Email,
+                PhoneNumber = dto.Phone,
+                //Mas adelante poner verificaicon de Email
+                EmailConfirmed = true
+            };
+            var result = await _userManager.CreateAsync(user, dto.Password!);
+            if (result.Succeeded)
+            {
+                var loginDto = new LoginRequestDto
+                {
+                    Email = dto.Email,
+                    Password = dto.Password
+                };
+                var responseAuthentication = await CreateToken(loginDto);
+                return responseAuthentication;
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return ValidationProblem();
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthenticationResponseDto>> Login(LoginRequestDto dto)
+        {
+            var user = await _userManager.FindByEmailAsync(dto.Email);
+            if (user is null) return ReturnLoginFalied();
+            var result = await _signInManager.CheckPasswordSignInAsync(user, dto.Password!, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return await CreateToken(dto);
+            }
+            else
+            {
+                return ReturnLoginFalied();
+            }
+        }
+
+        private ActionResult ReturnLoginFalied()
+        {
+            ModelState.AddModelError(string.Empty, "Login Incorrecto");
+            return ValidationProblem();
+        }
+
+        private async Task<AuthenticationResponseDto> CreateToken(LoginRequestDto dto)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("email", dto.Email)
+            };
+            //Ir a la db a buscar los claims
+            var user = await _userManager.FindByEmailAsync(dto.Email);
+            var claimsDB = await _userManager.GetClaimsAsync(user!);
+            claims.AddRange(claimsDB);
+            var keyJwt = Environment.GetEnvironmentVariable("key_jwt");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyJwt!));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expired = DateTime.UtcNow.AddHours(3);
+            var tokenSecurity = new JwtSecurityToken(issuer: null, audience: null, claims: claims, expires: expired,
+                signingCredentials: credentials);
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenSecurity);
+            return new AuthenticationResponseDto
+            {
+                Token = token,
+                Expired = expired
+            };
+        }
     }
 }
