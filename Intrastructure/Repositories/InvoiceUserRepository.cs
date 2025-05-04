e15a0ba881f57ac2000ea68ce08516513712256d
JhonMohamed <162652096+JhonMohamed@users.noreply.github.com>
2025-05-04 15:16:05 -0500

    Implement user report generation and related services

---
+++ Intrastructure/Repositories/InvoiceUserRepository.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Intrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intrastructure.Repositories
+{
+    public class InvoiceUserRepository : IInvoiceUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InvoiceUserRepository(ApplicationDbContext context)
+            => _context = context;
+
+        public async Task<List<Users>> ObtenerReporteAsync()
+        {
+            return await _context.Users
+                .ToListAsync();
+        }
+    }
+}
